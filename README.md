e4b97a1f7baab4ef86372d25d77067f7cd7cc89d
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2015-06-09 20:07:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Cyberouge
+=========
+
+turn any web page in to a dungeon complete with lore and loot
---
+++ cyberouge.py
@@ -0,0 +1,11 @@
+import requests
+import lxml.html as html
+
+def main():
+    print("welcome to cyberouge")
+
+def get_page():
+    pass
+
+if __name__ == "__main__":
+    main
