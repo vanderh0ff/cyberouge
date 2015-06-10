bf08b9bc2e2c7203f609aa4798b06e73060d30e1
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2015-06-09 21:46:29 -0400

    made get page
    added function to get web page for content generation

--- cyberouge.py
+++ cyberouge.py
@@ -4,8 +4,13 @@ import lxml.html as html
 def main():
     print("welcome to cyberouge")
 
-def get_page():
-    pass
+def get_page(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        html.fromstring(response.text)
+    else:
+        break
+
 
 if __name__ == "__main__":
-    main
+    main()
